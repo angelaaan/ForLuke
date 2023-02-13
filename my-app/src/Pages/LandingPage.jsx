03693cb090b51cb54d3193d7b007197870c6f595
angelaaan <114760476+angelaaan@users.noreply.github.com>
2023-02-13 13:05:57 -0500

    temp?

---
+++ my-app/src/Pages/LandingPage.jsx
@@ -0,0 +1,29 @@
+import './LandingPage.scss';
+
+function LandingPage() {
+
+    return (
+
+        <div class="body">
+            <h1>bruh</h1>
+            <div class="container">
+            <div class="envelope"></div>
+            <div class="card">
+
+                <h1 class="message">dear luke
+                    <br>happy valentines day</br>bro
+                </h1>
+                <div class="heart"></div>
+
+            </div>
+
+            <div class="cover"></div>
+            <div class="lid"></div>
+            <div class="shadow"></div>
+
+        </div>
+        </div>
+    )
+}
+
+export default LandingPage;
